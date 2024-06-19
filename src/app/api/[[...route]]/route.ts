a583202b0430c440fb6824e0842f1d14b3d74d3f
Caio <caio@localhost.localdomain>
2024-06-19 12:11:07 -0300

    header ready

---
+++ src/app/api/[[...route]]/route.ts
@@ -0,0 +1,13 @@
+import { z } from 'zod'
+import { Hono } from 'hono'
+import { handle } from 'hono/vercel'
+import { zValidator } from '@hono/zod-validator'
+
+export const runtime = 'edge'
+
+const app = new Hono().basePath('/api')
+
+app.get('/hello', (c) => c.json({ hello: 'hello' }))
+
+export const GET = handle(app)
+export const POST = handle(app)
