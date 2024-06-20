50329a81700f556e11647f63f04088676193ac7a
Caio <caio@localhost.localdomain>
2024-06-20 09:55:53 -0300

    api post need to verifiy why don't create account

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,10 @@
 import type { Metadata } from 'next'
 import { ClerkProvider } from '@clerk/nextjs'
 import './globals.css'
+
 import QueryProvider from '@/providers/query-provider'
+import { SheetProvider } from '@/providers/sheet-provider'
+import { Toaster } from '@/components/ui/sonner'
 
 export const metadata: Metadata = {
   title: 'Finance',
@@ -17,7 +20,11 @@ export default function RootLayout({
     <ClerkProvider>
       <html lang="en">
         <body>
-          <QueryProvider>{children}</QueryProvider>
+          <QueryProvider>
+            <SheetProvider />
+            <Toaster />
+            {children}
+          </QueryProvider>
         </body>
       </html>
     </ClerkProvider>
