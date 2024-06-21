5b162cfcccef9f552a2f0a43d251c6349384210e
Caio <caio@localhost.localdomain>
2024-06-21 00:38:06 -0300

    edit account and delete account ready to use

---
+++ src/features/accounts/api/use-delete-account.ts
@@ -0,0 +1,38 @@
+import { toast } from 'sonner'
+
+import { InferResponseType } from 'hono'
+import { useMutation, useQueryClient } from '@tanstack/react-query'
+
+import { client } from '@/lib/hono'
+
+type ResponseType = InferResponseType<
+  (typeof client.api.accounts)[':id']['$delete']
+>
+
+export const useDeleteAccount = (id?: string) => {
+  const queryClient = useQueryClient()
+
+  const mutation = useMutation<ResponseType, Error>({
+    mutationFn: async (json) => {
+      console.log('Sending request with json:', json)
+      const response = await client.api.accounts[':id'].$delete({
+        param: { id },
+      })
+      const data = await response.json()
+      console.log('received response:', data)
+      return data
+    },
+    onSuccess: () => {
+      toast.success('Account deleted')
+      queryClient.invalidateQueries({ queryKey: ['account', { id }] })
+      queryClient.invalidateQueries({ queryKey: ['accounts'] })
+      // TODO: Invalidate summary and transactions
+    },
+    onError: (err) => {
+      console.error('Faile to update', err)
+      toast.error('Failed to delete account')
+    },
+  })
+
+  return mutation
+}
---
+++ src/features/accounts/api/use-edit-account.ts
@@ -0,0 +1,42 @@
+import { toast } from 'sonner'
+
+import { InferRequestType, InferResponseType } from 'hono'
+import { useMutation, useQueryClient } from '@tanstack/react-query'
+
+import { client } from '@/lib/hono'
+
+type ResponseType = InferResponseType<
+  (typeof client.api.accounts)[':id']['$patch']
+>
+type RequestType = InferRequestType<
+  (typeof client.api.accounts)[':id']['$patch']
+>['json']
+
+export const useEditAccount = (id?: string) => {
+  const queryClient = useQueryClient()
+
+  const mutation = useMutation<ResponseType, Error, RequestType>({
+    mutationFn: async (json) => {
+      console.log('Sending request with json:', json)
+      const response = await client.api.accounts[':id'].$patch({
+        param: { id },
+        json,
+      })
+      const data = await response.json()
+      console.log('received response:', data)
+      return data
+    },
+    onSuccess: () => {
+      toast.success('Account updated')
+      queryClient.invalidateQueries({ queryKey: ['account', { id }] })
+      queryClient.invalidateQueries({ queryKey: ['accounts'] })
+      // TODO: Invalidate summary and transactions
+    },
+    onError: (err) => {
+      console.error('Faile to update', err)
+      toast.error('Failed to edit account')
+    },
+  })
+
+  return mutation
+}
