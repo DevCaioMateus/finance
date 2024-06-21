d208b9e5da7f58712be048e8291fcd5e3fea2332
Caio <caio@localhost.localdomain>
2024-06-21 19:49:33 -0300

    transactions history added

--- src/app/(dashboard)/transactions/page.tsx
+++ src/app/(dashboard)/transactions/page.tsx
@@ -3,8 +3,8 @@
 import { Loader2, Plus } from 'lucide-react'
 
 import { useNewTransaction } from '@/features/transactions/hooks/use-new-transactions'
-import { useGetAccounts } from '@/features/accounts/api/use-get-accounts'
-import { useBulkDeleteAccounts } from '@/features/accounts/api/use-bulk-delete-accounts'
+import { useGetTransactions } from '@/features/transactions/api/use-get-transactions'
+import { useBulkDeleteTransactions } from '@/features/transactions/api/use-bulk-delete-transactions'
 
 import { Button } from '@/components/ui/button'
 import { Skeleton } from '@/components/ui/skeleton'
@@ -15,14 +15,13 @@ import { columns } from './columns'
 
 const TransactionsPage = () => {
   const newTransaction = useNewTransaction()
-  const deleteAccounts = useBulkDeleteAccounts()
-  const accountsQuery = useGetAccounts()
-  console.log(accountsQuery.data)
-  const accounts = accountsQuery.data || []
+  const deleteTransactions = useBulkDeleteTransactions()
+  const transactionsQuery = useGetTransactions()
+  const transactions = transactionsQuery.data || []
 
-  const isDisabled = accountsQuery.isLoading || deleteAccounts.isPending
+  const isDisabled = transactionsQuery.isLoading || deleteTransactions.isPending
 
-  if (accountsQuery.isLoading) {
+  if (transactionsQuery.isLoading) {
     return (
       <div className="max-w-screen-2xl mx-auto w-full pb-10 -mt-24">
         <Card className="border-none drop-shadow-sm">
@@ -44,7 +43,7 @@ const TransactionsPage = () => {
       <Card className="border-none drop-shadow-sm">
         <CardHeader className="gap-y-2 lg:flex-row lg:item-center lg:justify-between">
           <CardTitle className="text-xl line-clamp-1">
-            Transactions Page
+            Transactions History
           </CardTitle>
           <Button size="sm" onClick={newTransaction.onOpen}>
             <Plus className="size-4 mr-2" />
@@ -54,11 +53,11 @@ const TransactionsPage = () => {
         <CardContent>
           <DataTable
             columns={columns}
-            data={accounts}
+            data={transactions}
             filterKey="name"
             onDelete={(row) => {
               const ids = row.map((r) => r.original.id)
-              deleteAccounts.mutate({ ids })
+              deleteTransactions.mutate({ ids })
             }}
             disabled={isDisabled}
           />
